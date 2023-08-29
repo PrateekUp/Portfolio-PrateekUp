362e9c23d4c2c3a921a8748334aa724c4737b632
Prateek Kumar <prateek30147@gmail.com>
2023-08-30 01:24:47 +0530

    minor bug fixes

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -22,7 +22,7 @@ const ProjectCard = ({
     >
       <Tilt
         options={{ max: 45, scale: 1, speed: 450 }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+        className="bg-tertiary p-2 sm:p-4 rounded-2xl sm:w-[360px] w-full"
       >
         <div className="relative w-full h-[230px]">
           <img
